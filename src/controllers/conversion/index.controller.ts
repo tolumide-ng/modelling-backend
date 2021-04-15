c266a12c430e23662bc000e5077158b50d184ae0
tolumide-ng <tolumideshopein@gmail.com>
2021-04-16 00:50:12 +0100

    handles controller interaction with saving aggregate

--- src/controllers/conversion/index.controller.ts
+++ src/controllers/conversion/index.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import { ResponseGenerator } from "../../helpers/responseGenerator";
-import db from "../../database/models";
+import { Upload } from "../../database/models/upload";
 import { BaseRepository } from "../../baseRepository";
 
 export class ConversionController extends ResponseGenerator {
-    static async uploadFile(req: Request, res: Response) {}
+    static async uploadFile(req: Request, res: Response) {
+        const { bucketUrl } = req;
+
+        const response = await BaseRepository.create(Upload, {
+            imageUrl: bucketUrl,
+        });
+
+        console.log("THE RESPOSNE FROM THE COMMAND REQUEST", response);
+    }
 }
--- src/database/models/upload.js
+++ src/database/models/upload.js
@@ -1,27 +1,26 @@
 "use strict";
 
-import Model from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../models";
 
-module.exports = (sequelize, DataTypes) => {
-    class Upload extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
+export class Upload extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+        // define association here
     }
-    Upload.init(
-        {
-            imageUrl: DataTypes.STRING,
-            imageId: DataTypes.UUID,
-        },
-        {
-            sequelize,
-            modelName: "Upload",
-        },
-    );
-    return Upload;
-};
+}
+Upload.init(
+    {
+        imageUrl: DataTypes.STRING,
+        imageId: DataTypes.UUID,
+    },
+    {
+        sequelize,
+        modelName: "Upload",
+    },
+);
+// return Upload;
