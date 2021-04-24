885518a0ce489775354efe62e0069af48cdeb253
tolumide-ng <tolumideshopein@gmail.com>
2021-04-24 04:44:53 +0100

    adds integration test for file download endpoint

--- src/tests/conversion/stream.endpoint.test.ts
+++ src/tests/conversion/stream.endpoint.test.ts
@@ -7,33 +7,21 @@ import { config } from "dotenv";
 import { BaseRepository } from "../../baseRepository";
 import Upload from "../../database/models/upload";
 import { ResponseGenerator } from "../../helpers/responseGenerator";
-import { Request, Response, NextFunction } from "express";
-import { SSEvents } from "../../controllers/conversion";
-import {
-    VALID_CONVERT_TARGETS,
-    NON_EXISTENT_ID,
-    INVALID_CONVERT_TARGET,
-} from "../utils";
+import { Request, Response } from "express";
+import { ConversionController } from "../../controllers/conversion";
+import { VALID_CONVERT_TARGETS, INVALID_CONVERT_TARGET } from "../utils";
 
 config();
 chai.use(chaiHttp);
 
 const server = () => chai.request(app);
 
-describe.only("GET /stream/:id", () => {
-    let res;
-
+describe("GET /stream/:id", () => {
     beforeEach(async () => {
         await Upload.destroy({ truncate: true });
     });
 
     it("should return an 404 status code", async () => {
-        // const TEST_FILE_NAME = "testFile.shapr";
-        // const response = await BaseRepository.create(Upload, {
-        //     fileUrl: "longFileUrlFromABucket",
-        //     fileName: TEST_FILE_NAME,
-        // });
-        // const { fileId } = await response.get({ plain: true });
         const res = await server().get(`/stream/${INVALID_CONVERT_TARGET}`);
 
         expect(res.status).to.equal(404);
@@ -43,7 +31,30 @@ describe.only("GET /stream/:id", () => {
         expect(currentTotalUploads.length).to.equal(0);
     });
 
-    it.only("should successfully convert the uploaded file", async () => {
-        // const stubSSE = sinon.stub(SSEvents, "")
+    it("should successfully convert the uploaded file", async () => {
+        const stubConversion = sinon
+            .stub(ConversionController, "streamConversion")
+            .callsFake(async (req: Request, res: Response) => {
+                return new Promise((resolve, reject) => {
+                    return ResponseGenerator.sendSuccess(res, 200, {
+                        message: "Success",
+                    });
+                });
+            });
+
+        const TEST_FILE_NAME = "testFile.shapr";
+        const response = await BaseRepository.create(Upload, {
+            fileUrl: "longFileUrlFromABucket",
+            fileName: TEST_FILE_NAME,
+            target: VALID_CONVERT_TARGETS[1],
+        });
+
+        const { fileId } = await response.get({ plain: true });
+
+        const res = await server().get(`/stream/${fileId}`);
+
+        expect(res.status).to.equal(200);
+
+        stubConversion.restore();
     });
 });
